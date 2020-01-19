3b87d8065f43f3ea501472d16f509ce32faf9391
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 04:30:00 +0100

    creacion metodos testCcuentaJSG y testNombreJSG

--- CCuenta/test/TestCCuentaJSG.java
+++ CCuenta/test/TestCCuentaJSG.java
@@ -6,8 +6,28 @@ public class TestCCuentaJSG {
 
 	@Test
 	public void testCcuentaJSG() {
+		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000,5);
+		assertNotNull(cuentaTest);
+	}
+
+	@Test
+	public void testNombreJSG() {
+		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000,5);
+		String nom = "Antonio";
+		String nombre = cuentaTest.getNombre();
+		assertSame(nom, nombre);
+	}
+
+	@Test
+	public void testCuentaJSG() {
 		fail("Not yet implemented");
 	}
+
+	@Test
+	public void testSaldoJSG() {
+		fail("Not yet implemented");
+	}
+
 
 	@Test
 	public void testIngresarJSG() {
