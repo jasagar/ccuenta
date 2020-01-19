33f17b1e53cece5ae0f0da65e75874245fa9a3bb
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 04:46:25 +0100

    añadido metodo testTresAtributosJSG()

--- CCuenta/test/TestCCuentaJSG.java
+++ CCuenta/test/TestCCuentaJSG.java
@@ -6,26 +6,22 @@ public class TestCCuentaJSG {
 
 	@Test
 	public void testCcuentaJSG() {
-		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000,5);
+		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000.0,5);
 		assertNotNull(cuentaTest);
 	}
 
 	@Test
-	public void testNombreJSG() {
-		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000,5);
+	public void testTresAtributosJSG() {
+		Ccuenta cuentaTest = new Ccuenta("Antonio","ES25-3006-6666-1115-8888",5000.0,5);
 		String nom = "Antonio";
 		String nombre = cuentaTest.getNombre();
-		assertSame(nom, nombre);
-	}
-
-	@Test
-	public void testCuentaJSG() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSaldoJSG() {
-		fail("Not yet implemented");
+		String cue = "ES25-3006-6666-1115-8888";
+		String cuenta = cuentaTest.getCuenta();
+		Double sal = 5000.0;
+		Double saldo = cuentaTest.getSaldo();
+		assertEquals(nom, nombre);
+		assertEquals(cue, cuenta);
+		assertEquals(sal, saldo);
 	}
 
 
