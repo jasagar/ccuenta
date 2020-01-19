775fc95f265442d2617fdcd9574b86cdaa2a242a
Jaime Sansano Garcia <jasano2@hotmail.com>
2020-01-19 03:57:46 +0100

    creacion carpeta test y clase TestCCuentaJSG.java

---
+++ CCuenta/test/TestCCuentaJSG.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCCuentaJSG {
+
+	@Test
+	public void testCcuenta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIngresar() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRetirar() {
+		fail("Not yet implemented");
+	}
+
+}
